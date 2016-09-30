0bb0da833abee835161f3da48fa4edc6cb459288
lvan138 <lvan1381996@gmail.com>
2016-10-01 00:35:06 +0700

    Update contact.js
    Thay kinh độ vĩ độ.title,infoWindow: {
    		                content: "Trường Đại học Công Nghệ <br> Đại học Quốc gia Hà Nội"
    		            }

--- assets/pages/scripts/contact.js
+++ assets/pages/scripts/contact.js
@@ -7,15 +7,15 @@ var Contact = function () {
 			$(document).ready(function(){
 			  map = new GMaps({
 				div: '#gmapbg',
-				lat: -13.004333,
-				lng: -38.494333
+				lat: 21.0384376,
+				lng: 105.7817945
 			  });
 			   var marker = map.addMarker({
-		            lat: -13.004333,
-					lng: -38.494333,
-		            title: 'Loop, Inc.',
+		          lat: 21.0384376,
+				lng: 105.7817945,
+		            title: 'UET-VNU.',
 		            infoWindow: {
-		                content: "<b>Metronic, Inc.</b> 795 Park Ave, Suite 120<br>San Francisco, CA 94107"
+		                content: "Trường Đại học Công Nghệ <br> Đại học Quốc gia Hà Nội"
 		            }
 		        });
 
